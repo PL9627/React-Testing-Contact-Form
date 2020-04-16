0545e858d7af656f49381c084a5a5968690d8dcc
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-16 16:57:12 -0700

    2nd test did not pass. fixed issue w/ first name label and test passed. Will add other labels to 2nd test to see if they have the same problem.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {render, fireEvent, findAllByTestId} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('renders ContactForm without crashing', () => {
-    render(<ContactForm />)
+    render(<ContactForm />);
 });
 
+test('first name label will display', () => {
+    const {getByLabelText} = render(<ContactForm />);
+
+    const fnLabel = getByLabelText(/first name/i);
+})
